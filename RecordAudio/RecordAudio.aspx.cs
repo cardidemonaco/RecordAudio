6d1b6d61db0125f1ae5d731b14bd09db6ad54041
Cardi DeMonaco Jr <cardi@rokpte.com>
2018-05-09 14:35:48 -0400

    Initial Commit
    Recording Audio!!!

---
+++ RecordAudio/RecordAudio.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//Example of recording audio, using Edge 
+//(could not get Chrome to work - has to do with OS permission I believe)
+//https://webaudiodemos.appspot.com/AudioRecorder/index.html
+
+namespace RecordAudio
+{
+    public partial class RecordAudio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
